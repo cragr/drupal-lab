94749518db7bfb05c31bb0aa264d1e67fbce4776
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2020-11-14 16:51:45 -0800

    Revert "Issue #3182188: removes vendor cleanup from config to test"
    This reverts commit 0cab794a8d8cbad9e52b2eac3e202e3def662476.

--- composer/Plugin/VendorHardening/Config.php
+++ composer/Plugin/VendorHardening/Config.php
@@ -24,6 +24,7 @@ class Config {
     'behat/mink-browserkit-driver' => ['tests'],
     'behat/mink-goutte-driver' => ['tests'],
     'behat/mink-selenium2-driver' => ['tests'],
+    'composer/composer' => ['bin'],
     'drupal/coder' => [
       'coder_sniffer/Drupal/Test',
       'coder_sniffer/DrupalPractice/Test',
