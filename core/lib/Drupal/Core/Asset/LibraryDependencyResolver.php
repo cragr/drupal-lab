0eff267f4455f00f2118ad205c7f1fee920ec42d
Dieter Holvoet <dieter.holvoet@gmail.com>
2020-11-16 15:15:03 +0100

    Add validation to ensure libraries are in the right format

--- core/lib/Drupal/Core/Asset/LibraryDependencyResolver.php
+++ core/lib/Drupal/Core/Asset/LibraryDependencyResolver.php
@@ -49,14 +49,19 @@ class LibraryDependencyResolver implements LibraryDependencyResolverInterface {
    */
   protected function doGetDependencies(array $libraries_with_unresolved_dependencies, array $final_libraries = []) {
     foreach ($libraries_with_unresolved_dependencies as $library) {
-      if (!in_array($library, $final_libraries)) {
-        list($extension, $name) = explode('/', $library, 2);
-        $definition = $this->libraryDiscovery->getLibraryByName($extension, $name);
-        if (!empty($definition['dependencies'])) {
-          $final_libraries = $this->doGetDependencies($definition['dependencies'], $final_libraries);
-        }
-        $final_libraries[] = $library;
+      if (in_array($library, $final_libraries)) {
+        continue;
       }
+      $parts = explode('/', $library, 2);
+      if (count($parts) !== 2) {
+        continue;
+      }
+      list($extension, $name) = $parts;
+      $definition = $this->libraryDiscovery->getLibraryByName($extension, $name);
+      if (!empty($definition['dependencies'])) {
+        $final_libraries = $this->doGetDependencies($definition['dependencies'], $final_libraries);
+      }
+      $final_libraries[] = $library;
     }
     return $final_libraries;
   }
