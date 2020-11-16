942c9281d041416fcefeaf44b1d41646cde23752
Claudiu Cristea <clau.cristea@gmail.com>
2020-11-16 18:07:40 +0200

    Add deprecation test coverage.

--- core/tests/Drupal/Tests/Core/Entity/EntityBundleAccessCheckTest.php
+++ core/tests/Drupal/Tests/Core/Entity/EntityBundleAccessCheckTest.php
@@ -94,4 +94,14 @@ class EntityBundleAccessCheckTest extends UnitTestCase {
     $this->assertEquals($access_result, $access_check->access($route, $route_match, $account));
   }
 
+  /**
+   * Tests service class deprecation.
+   *
+   * @group legacy
+   */
+  public function testServiceDeprecation(): void {
+    $this->expectDeprecation('The Drupal\Core\Entity\EntityBundleAccessCheck is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Instead, specify the list of bundles in the entity parameter, under "bundle" key, as a sequence. See https://www.drupal.org/node/3155569');
+    new EntityBundleAccessCheck();
+  }
+
 }
