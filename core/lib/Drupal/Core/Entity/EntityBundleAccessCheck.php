b6fb152f655ede9834fc12b60b4e35ed31a1022b
Claudiu Cristea <clau.cristea@gmail.com>
2020-11-17 21:13:54 +0200

    Use @todo instead of @deprecated in class

--- core/lib/Drupal/Core/Entity/EntityBundleAccessCheck.php
+++ core/lib/Drupal/Core/Entity/EntityBundleAccessCheck.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Route;
 /**
  * Provides an entity bundle checker for the _entity_bundles route requirement.
  *
- * @deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Instead,
+ * @todo Deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Instead,
  *   specify the list of bundles in the entity parameter, under "bundle" key, as
  *   a sequence.
  *
