317a561a371a130a9ad9151589d7334e256aad2e
Claudiu Cristea <clau.cristea@gmail.com>
2020-11-17 11:57:25 +0200

    Move the deprecation trigger inside the class constructor.

--- core/lib/Drupal/Core/Entity/EntityBundleAccessCheck.php
+++ core/lib/Drupal/Core/Entity/EntityBundleAccessCheck.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\Core\Entity;
 
-@trigger_error('The ' . __NAMESPACE__ . '\EntityBundleAccessCheck is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Instead, specify the list of bundles in the entity parameter, under "bundle" key, as a sequence. See https://www.drupal.org/node/3155569', E_USER_DEPRECATED);
-
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Routing\Access\AccessInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
@@ -20,6 +18,13 @@ use Symfony\Component\Routing\Route;
  * @see https://www.drupal.org/node/3155569
  */
 class EntityBundleAccessCheck implements AccessInterface {
+
+  /**
+   * Constructs a new access check instance.
+   */
+  public function __construct() {
+    @trigger_error('The ' . __NAMESPACE__ . '\EntityBundleAccessCheck is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Instead, specify the list of bundles in the entity parameter, under "bundle" key, as a sequence. See https://www.drupal.org/node/3155569', E_USER_DEPRECATED);
+  }
 
   /**
    * Checks entity bundle match based on the _entity_bundles route requirement.
