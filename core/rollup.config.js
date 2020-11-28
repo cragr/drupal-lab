1ff14176cb5ef0ec73daae594b99d5b111a42567
Théodore BIADALA <theodore@biadala.net>
2020-11-28 02:41:17 +0100

    simpler

--- core/rollup.config.js
+++ core/rollup.config.js
@@ -11,31 +11,7 @@ const def = {
 }
 
 export default [
-  //addAsset('backbone', { unminified: 'backbone.js', minified: 'backbone-min.js' }),
-  //addAsset('underscore', { unminified: 'underscore.js', minified: 'underscore-min.js', 'import': 'underscore/underscore', 'name': '_' }, ([step1, step2]) => {
 
-  // ok.
-  //addAsset('js-cookie', { unminified: 'js.cookie.js', 'import': 'js-cookie', 'name': 'Cookies', 'export': true }, ([step1, step2]) => {
-  //  // Prevent leaking variables.
-  //  step2.output.format = 'iife';
-  //  return [step1, step2];
-  //}),
-  //addAsset('popperjs', { unminified: 'popper.js', 'import': '@popperjs/core/dist/umd/popper', 'name': 'Popper' }, ([step1, step2]) => {
-  //  // Prevent leaking variables.
-  //  step2.output.format = 'iife';
-  //  return [step1, step2];
-  //}),
-  //addAsset('jquery-form', { unminified: 'jquery.form.js', 'import': 'jquery-form/dist/jquery.form.min.js', 'export': 'jqueryForm' },  ([step1, step2]) =>  [
-  //  {
-  //    ...step1,
-  //    output: {
-  //      // Sortable is already minified, no need to reminify it.
-  //      ...step2.output,
-  //      sourcemap: false,
-  //    }
-  //  }
-  //]),
-//
   //addAsset('sortable', { unminified: 'Sortable.js', 'import': 'sortablejs/dist/sortable.umd', 'name': 'Sortable' }, ([step1, step2]) => [
   //  {
   //    ...step1,
@@ -48,11 +24,139 @@ export default [
   //]),
 
 
+  // SortableJS.
+  [
+    {
+      ...def,
+      output: { file: 'assets/vendor/sortable/Sortable.js' },
+      plugins: [
+        virtual({ entry: 'import "sortablejs";' }),
+        resolve()
+      ],
+    },
+    {
+      ...def,
+      input: 'assets/vendor/sortable/Sortable.js',
+      output: [
+        {
+          file: 'assets/vendor/sortable/Sortable.min.js',
+          sourcemap: true,
+        },
+      ],
+      plugins: [terser({
+        //compress: { inline: false },
+        format: { comments: false },
+      })],
+    }
+  ],
+
+  // jquery-form.
+  [
+    {
+      ...def,
+      output: { file: 'assets/vendor/jquery-form/jquery.form.js' },
+      plugins: [
+        virtual({ entry: 'import "jquery-form";' }),
+        resolve()
+      ],
+    },
+    {
+      ...def,
+      input: 'assets/vendor/jquery-form/jquery.form.js',
+      output: [
+        {
+          file: 'assets/vendor/jquery-form/jquery.form.min.js',
+          sourcemap: true,
+        },
+      ],
+      plugins: [terser({
+        compress: { inline: false },
+        format: { comments: false },
+      })],
+    }
+  ],
+
+  // PopperJS.
+  [
+    {
+      ...def,
+      output: { file: 'assets/vendor/popperjs/popper.js' },
+      plugins: [
+        virtual({ entry: 'import "@popperjs/core/dist/umd/popper";' }),
+        resolve()
+      ],
+    },
+    {
+      ...def,
+      input: 'assets/vendor/popperjs/popper.js',
+      output: [
+        {
+          file: 'assets/vendor/popperjs/popper.min.js',
+          sourcemap: true,
+          format: 'iife',
+          name: 'Popper',
+        },
+      ],
+      plugins: [terser({
+        compress: { inline: false },
+        format: { comments: false },
+      })],
+    }
+  ],
+
+  // js-cookie.
+  [
+    {
+      ...def,
+      output: { file: 'assets/vendor/js-cookie/js.cookie.js' },
+      plugins: [
+        virtual({ entry: 'import Cookies from "js-cookie"; export default Cookies;' }),
+        resolve()
+      ],
+    },
+    {
+      ...def,
+      input: 'assets/vendor/js-cookie/js.cookie.js',
+      output: [
+        {
+          file: 'assets/vendor/js-cookie/js.cookie.min.js',
+          sourcemap: true,
+          format: 'iife',
+          name: 'Cookies',
+        },
+      ],
+      plugins: [terser({ format: { comments: false } })],
+    }
+  ],
+
+  // underscore
+  [
+    {
+      ...def,
+      output: { file: 'assets/vendor/underscore/underscore.js' },
+      plugins: [
+        virtual({ entry: 'import "underscore/underscore";' }),
+        resolve()
+      ],
+    },
+    {
+      ...def,
+      input: 'assets/vendor/underscore/underscore.js',
+      output: [
+        {
+          file: 'assets/vendor/underscore/underscore-min.js',
+          sourcemap: true,
+        },
+      ],
+      plugins: [terser({ format: { comments: false } })],
+    }
+  ],
+
   // Backbone
   [
     {
       ...def,
-      output: { file: 'assets/vendor/backbone.js' },
+      output: { file: 'assets/vendor/backbone/backbone.js' },
       plugins: [
         virtual({ entry: 'import "backbone";' }),
         resolve()
