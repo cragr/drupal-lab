f34de7d28b065c2852ea1266a67a63649b73e1ab
JohnAlbin <virtually.johnalbin@gmail.com>
2020-11-29 23:11:25 +0800

    Refactor TwigDebugMarkupTest

--- core/modules/system/tests/src/Functional/Theme/TwigDebugMarkupTest.php
+++ core/modules/system/tests/src/Functional/Theme/TwigDebugMarkupTest.php
@@ -17,7 +17,7 @@ class TwigDebugMarkupTest extends BrowserTestBase {
    *
    * @var array
    */
-  protected static $modules = ['theme_test', 'node'];
+  protected static $modules = ['theme_test'];
 
   /**
    * {@inheritdoc}
@@ -25,172 +25,161 @@ class TwigDebugMarkupTest extends BrowserTestBase {
   protected $defaultTheme = 'stark';
 
   /**
-   * Shared renderer for each test.
-   *
-   * @var \Drupal\Core\Render\RendererInterface
-   */
-  protected $renderer;
-
-  /**
    * {@inheritdoc}
    */
   protected function setUp():void {
     parent::setUp();
 
-    \Drupal::service('theme_installer')->install(['test_theme']);
-    $this->config('system.theme')->set('default', 'test_theme')->save();
-    $this->drupalCreateContentType(['type' => 'page']);
-
-    // Enable debug, rebuild the service container, and clear all caches.
+    // Enable Twig debug, rebuild the service container, and clear all caches.
     $parameters = $this->container->getParameter('twig.config');
     $parameters['debug'] = TRUE;
     $this->setContainerParameter('twig.config', $parameters);
     $this->rebuildContainer();
     $this->resetAll();
-
-    $this->renderer = $this->container->get('renderer');
   }
 
   /**
-   * Tests debug markup added to Twig template output.
+   * Helper function to convert a render array to markup.
+   *
+   * @param array $elements
+   *   The structured array describing the data to be rendered.
+   *
+   * @return string
+   *   The rendered markup.
    */
-  public function testTwigDebugMarkup() {
+  public function render(array $elements) {
+    /* @var \Drupal\Core\Render\Renderer $renderer */
+    $renderer = $this->container->get('renderer');
+    return $renderer->renderRoot($elements);
+  }
+
+  /**
+   * Tests debug markup is on and off.
+   */
+  public function testDebugMarkup() {
     $extension = twig_extension();
-    $xss_suggestion = Html::escape('node--<script type="text/javascript">alert(\'yo\');</script>') . $extension;
+
+    // Find full path to template.
     $cache = $this->container->get('theme.registry')->get();
-    // Create array of Twig templates.
-    $templates = drupal_find_theme_templates($cache, $extension, drupal_get_path('theme', 'test_theme'));
-    $templates += drupal_find_theme_templates($cache, $extension, drupal_get_path('module', 'node'));
+    $templates = drupal_find_theme_templates($cache, $extension, drupal_get_path('module', 'theme_test'));
+    $template_filename = $templates['theme_test_specific_suggestions']['path'] . '/' . $templates['theme_test_specific_suggestions']['template'] . $extension;
 
-    // Create a node and test different features of the debug markup.
-    $node = $this->drupalCreateNode();
-    $builder = \Drupal::entityTypeManager()->getViewBuilder('node');
-    $build = $builder->view($node);
-    $output = $this->renderer->renderRoot($build);
-    $this->assertStringContainsString('<!-- THEME DEBUG -->', $output, 'Twig debug markup found in theme output when debug is enabled.');
-    $this->assertStringContainsString("THEME HOOK: 'node'", $output, 'Theme call information found.');
-    $expected = '<!-- FILE NAME SUGGESTIONS:' . PHP_EOL
-      . '   * ' . $xss_suggestion . PHP_EOL
-      . '   * node--1--full' . $extension . PHP_EOL
-      . '   x node--1' . $extension . PHP_EOL
-      . '   * node--page--full' . $extension . PHP_EOL
-      . '   * node--page' . $extension . PHP_EOL
-      . '   * node--full' . $extension . PHP_EOL
-      . '   * node' . $extension . PHP_EOL
-      . '-->';
-    $this->assertStringContainsString($expected, $output, 'Suggested template files found in order and node ID specific template shown as current template.');
-    $template_filename = $templates['node__1']['path'] . '/' . $templates['node__1']['template'] . $extension;
-    $this->assertStringContainsString("BEGIN OUTPUT from '$template_filename'", $output, 'Full path to current template file found.');
+    // Render a template.
+    $build = [
+      '#theme' => 'theme_test_specific_suggestions',
+    ];
+    $output = $this->render($build);
 
-    // Create another node and make sure the template suggestions shown in the
-    // debug markup are correct.
-    $node2 = $this->drupalCreateNode();
-    $build = $builder->view($node2);
-    $output = $this->renderer->renderRoot($build);
-    $expected = '<!-- FILE NAME SUGGESTIONS:' . PHP_EOL
-      . '   * ' . $xss_suggestion . PHP_EOL
-      . '   * node--2--full' . $extension . PHP_EOL
-      . '   * node--2' . $extension . PHP_EOL
-      . '   * node--page--full' . $extension . PHP_EOL
-      . '   * node--page' . $extension . PHP_EOL
-      . '   * node--full' . $extension . PHP_EOL
-      . '   x node' . $extension . PHP_EOL
-      . '-->';
-    $this->assertStringContainsString($expected, $output, 'Suggested template files found in order and base template shown as current template.');
+    $expected = '<!-- THEME DEBUG -->';
+    $this->assertStringContainsString($expected, $output, 'Twig debug markup found in theme output when debug is enabled.');
 
-    // Create another node and make sure the template suggestions shown in the
-    // debug markup are correct.
-    $node3 = $this->drupalCreateNode();
-    $build = ['#theme' => 'node__foo__bar'];
-    $build += $builder->view($node3);
-    $output = $this->renderer->renderRoot($build);
-    $this->assertStringContainsString("THEME HOOK: 'node__foo__bar'", $output, 'Theme call information found.');
-    $expected = '<!-- FILE NAME SUGGESTIONS:' . PHP_EOL
-      . '   * ' . $xss_suggestion . PHP_EOL
-      . '   * node--3--full' . $extension . PHP_EOL
-      . '   * node--3' . $extension . PHP_EOL
-      . '   * node--page--full' . $extension . PHP_EOL
-      . '   * node--page' . $extension . PHP_EOL
-      . '   * node--full' . $extension . PHP_EOL
-      . '   * node--foo--bar' . $extension . PHP_EOL
-      . '   * node--foo' . $extension . PHP_EOL
-      . '   x node' . $extension . PHP_EOL
-      . '-->';
-    $this->assertStringContainsString($expected, $output, 'Suggested template files found in order and base template shown as current template.');
+    $expected = "\n<!-- THEME HOOK: 'theme_test_specific_suggestions' -->";
+    $this->assertStringContainsString($expected, $output, 'Theme hook comment found.');
 
-    // Disable debug, rebuild the service container, and clear all caches.
+    $expected = "\n<!-- BEGIN OUTPUT from '" . Html::escape($template_filename) . "' -->\n";
+    $this->assertStringContainsString($expected, $output, 'Full path to current template file found in BEGIN OUTPUT comment.');
+    $expected = "\n<!-- END OUTPUT from '" . Html::escape($template_filename) . "' -->\n";
+    $this->assertStringContainsString($expected, $output, 'Full path to current template file found in END OUTPUT comment.');
+
+    // Disable Twig debug, rebuild the service container, and clear all caches.
     $parameters = $this->container->getParameter('twig.config');
     $parameters['debug'] = FALSE;
     $this->setContainerParameter('twig.config', $parameters);
     $this->rebuildContainer();
     $this->resetAll();
 
-    $build = $builder->view($node);
-    $output = $this->renderer->renderRoot($build);
-    $this->assertStringNotContainsString('<!-- THEME DEBUG -->', $output, 'Twig debug markup not found in theme output when debug is disabled.');
+    // Re-render the template.
+    $output = $this->render($build);
+
+    $expected = 'Template for testing specific theme calls.';
+    $this->assertStringContainsString($expected, $output, 'Confirm template is still rendered.');
+    $unexpected = '<!-- THEME DEBUG -->';
+    $this->assertStringNotContainsString($unexpected, $output, 'Twig debug markup not found in theme output when debug is disabled.');
   }
 
   /**
-   * Tests debug markup for array suggestions and hook_theme_suggestions_HOOK().
+   * Tests file name suggestions comment.
    */
-  public function testArraySuggestionsTwigDebugMarkup() {
-    \Drupal::service('module_installer')->install(['theme_suggestions_test']);
+  public function testFileNameSuggestions() {
     $extension = twig_extension();
-    $this->drupalGet('theme-test/array-suggestions');
-    $output = $this->getSession()->getPage()->getContent();
 
-    $expected = "THEME HOOK: 'theme_test_array_suggestions__implemented'";
-    $this->assertStringContainsString($expected, $output, 'Theme call information found.');
+    // Render a template using a single suggestion.
+    $build = [
+      '#theme' => 'theme_test_specific_suggestions',
+    ];
+    $output = $this->render($build);
 
+    $expected = "\n<!-- THEME HOOK: 'theme_test_specific_suggestions' -->";
+    $this->assertStringContainsString($expected, $output, 'Theme hook comment found.');
+    $unexpected = '<!-- FILE NAME SUGGESTIONS:';
+    $this->assertStringNotContainsString($unexpected, $output, 'A single suggestion should not have file name suggestions listed.');
+
+    // Render a template using multiple suggestions.
+    $build = [
+      '#theme' => 'theme_test_specific_suggestions__variant',
+    ];
+    $output = $this->render($build);
+
+    $expected = "\n<!-- THEME HOOK: 'theme_test_specific_suggestions__variant' -->";
+    $this->assertStringContainsString($expected, $output, 'Theme hook comment found.');
     $expected = '<!-- FILE NAME SUGGESTIONS:' . PHP_EOL
-      . '   * theme-test-array-suggestions--from-hook-theme-suggestions-hook-alter' . $extension . PHP_EOL
-      . '   * theme-test-array-suggestions--implemented--not-implemented' . $extension . PHP_EOL
-      . '   x theme-test-array-suggestions--implemented' . $extension . PHP_EOL
-      . '   * theme-test-array-suggestions--not-implemented' . $extension . PHP_EOL
-      . '   * theme-test-array-suggestions--from-hook-theme-suggestions-hook' . $extension . PHP_EOL
-      . '   * theme-test-array-suggestions' . $extension . PHP_EOL
-      . '-->';
-    $message = 'Suggested template files found in order and correct suggestion shown as current template.';
-    $this->assertStringContainsString($expected, $output, $message);
-  }
-
-  /**
-   * Tests debug markup for specific suggestions without implementation.
-   */
-  public function testUnimplementedSpecificSuggestionsTwigDebugMarkup() {
-    $extension = twig_extension();
-    $this->drupalGet('theme-test/specific-suggestion-not-found');
-    $output = $this->getSession()->getPage()->getContent();
-
-    $expected = "THEME HOOK: 'theme_test_specific_suggestions__variant_not_found__too'";
-    $this->assertStringContainsString($expected, $output, 'Theme call information found.');
-
-    $message = 'Suggested template files found in order and base template shown as current template.';
-    $expected = '<!-- FILE NAME SUGGESTIONS:' . PHP_EOL
-      . '   * theme-test-specific-suggestions--variant-not-found--too' . $extension . PHP_EOL
-      . '   * theme-test-specific-suggestions--variant-not-found' . $extension . PHP_EOL
+      . '   * theme-test-specific-suggestions--variant' . $extension . PHP_EOL
       . '   x theme-test-specific-suggestions' . $extension . PHP_EOL
       . '-->';
-    $this->assertStringContainsString($expected, $output, $message);
+    $this->assertStringContainsString($expected, $output, 'Multiple suggestions should have file name suggestions listed.');
   }
 
   /**
-   * Tests debug markup for specific suggestions.
+   * Tests suggestions when file name does not match.
    */
-  public function testSpecificSuggestionsTwigDebugMarkup() {
+  public function testFileNameNotMatchingSuggestion() {
     $extension = twig_extension();
-    $this->drupalGet('theme-test/specific-suggestion-alter');
-    $output = $this->getSession()->getPage()->getContent();
 
-    $expected = "THEME HOOK: 'theme_test_specific_suggestions__variant'";
-    $this->assertStringContainsString($expected, $output, 'Theme call information found.');
+    // Find full path to template.
+    $cache = $this->container->get('theme.registry')->get();
+    $templates = drupal_find_theme_templates($cache, $extension, drupal_get_path('module', 'theme_test'));
+    $template_filename = $templates['theme_test_template_test']['path'] . '/' . $templates['theme_test_template_test']['template'] . $extension;
+
+    // Render a template that doesn't match its suggestion name.
+    $build = [
+      '#theme' => 'theme_test_template_test__variant',
+    ];
+    $output = $this->render($build);
+
+    $expected = "\n<!-- THEME HOOK: 'theme_test_template_test__variant' -->";
+    $this->assertStringContainsString($expected, $output, 'Theme hook comment found.');
 
     $expected = '<!-- FILE NAME SUGGESTIONS:' . PHP_EOL
-      . '   x theme-test-specific-suggestions--variant' . $extension . PHP_EOL
-      . '   * theme-test-specific-suggestions' . $extension . PHP_EOL
+      . '   * theme-test-template-test--variant' . $extension . PHP_EOL
+      . '   x theme_test.template_test' . $extension . PHP_EOL
       . '-->';
-    $message = 'Suggested template files found in order and suggested template shown as current.';
-    $this->assertStringContainsString($expected, $output, $message);
+    $this->assertStringContainsString($expected, $output, 'The actual template file name should be used when it does not match the suggestion.');
+
+    $expected = "\n<!-- BEGIN OUTPUT from '" . Html::escape($template_filename) . "' -->\n";
+    $this->assertStringContainsString($expected, $output, 'Full path to current template file found in BEGIN OUTPUT comment.');
+  }
+
+  /**
+   * Tests XSS attempt in theme suggestions and Twig debug comments.
+   */
+  public function testXssComments() {
+    $extension = twig_extension();
+
+    // Render a template whose suggestions have been compromised.
+    $build = [
+      '#theme' => 'theme_test_xss_suggestion',
+    ];
+    $output = $this->render($build);
+
+    // @see theme_test_theme_suggestions_node()
+    $xss_suggestion = Html::escape('theme-test-xss-suggestion--<script type="text/javascript">alert(\'yo\');</script>') . $extension;
+
+    $expected = '<!-- FILE NAME SUGGESTIONS:' . PHP_EOL
+      . '   * ' . $xss_suggestion . PHP_EOL
+      . '   x theme-test-xss-suggestion' . $extension . PHP_EOL
+      . '-->';
+    $this->assertStringContainsString($expected, $output, 'XSS suggestion successfully escaped in Twig debug comments.');
+    $this->assertStringContainsString('Template for testing XSS in theme hook suggestions.', $output, 'Base hook suggestion used instead of XSS suggestion.');
   }
 
 }
