84275b56e585147828364c9ebb9ff6b905cce502
Théodore BIADALA <theodore@biadala.net>
2020-11-30 16:19:39 +0100

    Keep version + licence comments

--- core/rollup.config.js
+++ core/rollup.config.js
@@ -11,6 +11,8 @@ const def = {
 
 export default [
 
+  /* ES Modules */
+
   // once.
   /*
   [
@@ -39,25 +41,82 @@ export default [
     }
   ],*/
 
-  // normalize.css
+  // PopperJS.
   [
     {
       ...def,
-      output: { file: '' },
+      output: { file: 'assets/vendor/popperjs/popper.js' },
       plugins: [
-        virtual({ entry: '' }),
-        resolve(),
-        copy({
-          targets: [
-            {
-              src: 'node_modules/normalize.css/normalize.css',
-              dest: 'assets/vendor/normalize-css',
-            }
-          ],
-        }),
+        virtual({ entry: 'import "@popperjs/core/dist/umd/popper";' }),
+        resolve()
       ],
     },
+    {
+      ...def,
+      input: 'assets/vendor/popperjs/popper.js',
+      output: [
+        {
+          file: 'assets/vendor/popperjs/popper.min.js',
+          sourcemap: true,
+          format: 'iife',
+          name: 'Popper',
+        },
+      ],
+      plugins: [terser({ compress: { inline: false } })],
+    }
   ],
+
+  // SortableJS.
+  [
+    {
+      ...def,
+      output: { file: 'assets/vendor/sortable/Sortable.js' },
+      plugins: [
+        virtual({ entry: 'import Sortable from "sortablejs/modular/sortable.complete.esm"; export default Sortable;' }),
+        resolve()
+      ],
+    },
+    {
+      ...def,
+      input: 'assets/vendor/sortable/Sortable.js',
+      output: [
+        {
+          file: 'assets/vendor/sortable/Sortable.min.js',
+          sourcemap: true,
+          format: 'iife',
+          name: 'Sortable',
+        },
+      ],
+      plugins: [terser()],
+    }
+  ],
+
+  // js-cookie.
+  [
+    {
+      ...def,
+      output: { file: 'assets/vendor/js-cookie/js.cookie.js' },
+      plugins: [
+        virtual({ entry: 'import Cookies from "js-cookie"; export default Cookies;' }),
+        resolve()
+      ],
+    },
+    {
+      ...def,
+      input: 'assets/vendor/js-cookie/js.cookie.js',
+      output: [
+        {
+          file: 'assets/vendor/js-cookie/js.cookie.min.js',
+          sourcemap: true,
+          format: 'iife',
+          name: 'Cookies',
+        },
+      ],
+      plugins: [terser()],
+    }
+  ],
+
+  /* UMD source */
 
   // es6-promise.
   [
@@ -78,11 +137,153 @@ export default [
           sourcemap: true,
         },
       ],
-      plugins: [terser({
-        compress: { inline: false },
-        format: { comments: false },
-      })],
+      plugins: [terser({ compress: { inline: false } })],
     }
+  ],
+  // jquery-form.
+  [
+    {
+      ...def,
+      output: { file: 'assets/vendor/jquery-form/jquery.form.js' },
+      plugins: [
+        virtual({ entry: 'import "jquery-form";' }),
+        resolve()
+      ],
+    },
+    {
+      ...def,
+      input: 'assets/vendor/jquery-form/jquery.form.js',
+      output: [
+        {
+          file: 'assets/vendor/jquery-form/jquery.form.min.js',
+          sourcemap: true,
+        },
+      ],
+      plugins: [terser({ compress: { inline: false } })],
+    }
+  ],
+
+  // underscore
+  [
+    {
+      ...def,
+      output: { file: 'assets/vendor/underscore/underscore.js' },
+      plugins: [
+        virtual({ entry: 'import "underscore/underscore";' }),
+        resolve()
+      ],
+    },
+    {
+      ...def,
+      input: 'assets/vendor/underscore/underscore.js',
+      output: [
+        {
+          file: 'assets/vendor/underscore/underscore-min.js',
+          sourcemap: true,
+        },
+      ],
+      plugins: [terser()],
+    }
+  ],
+
+  // Backbone
+  [
+    {
+      ...def,
+      output: { file: 'assets/vendor/backbone/backbone.js' },
+      plugins: [
+        virtual({ entry: 'import "backbone";' }),
+        resolve()
+      ],
+    },
+    {
+      ...def,
+      input: 'assets/vendor/backbone/backbone.js',
+      output: [
+        {
+          file: 'assets/vendor/backbone/backbone-min.js',
+          sourcemap: true,
+        },
+      ],
+      plugins: [terser()],
+    }
+  ],
+
+  // Picturefill.
+  [
+    {
+      ...def,
+      output: { file: 'assets/vendor/picturefill/picturefill.js' },
+      plugins: [
+        virtual({ entry: 'import "picturefill";' }),
+        resolve(),
+      ],
+    },
+    {
+      ...def,
+      input: 'assets/vendor/picturefill/picturefill.js',
+      output: [
+        {
+          file: 'assets/vendor/picturefill/picturefill.min.js',
+          sourcemap: true,
+        },
+      ],
+      plugins: [terser()],
+    },
+  ],
+
+  // jQuery
+  [
+    {
+      ...def,
+      output: { file: 'assets/vendor/jquery/jquery.js' },
+      plugins: [
+        virtual({ entry: 'import "jquery";' }),
+        resolve()
+      ],
+    },
+    {
+      ...def,
+      input: 'assets/vendor/jquery/jquery.js',
+      output: [
+        {
+          file: 'assets/vendor/jquery/jquery.min.js',
+          sourcemap: true,
+        },
+      ],
+      plugins: [
+        terser({
+          // Set compression options to be closer to jquery default output.
+          compress: {
+            evaluate: false,
+            inline: false,
+            passes: 2,
+          },
+        }),
+      ],
+    }
+  ],
+
+  /* Special cases */
+
+  // normalize.css
+  [
+    {
+      ...def,
+      output: { file: '' },
+      plugins: [
+        virtual({ entry: '' }),
+        resolve(),
+        copy({
+          targets: [
+            {
+              src: 'node_modules/normalize.css/normalize.css',
+              dest: 'assets/vendor/normalize-css',
+            }
+          ],
+        }),
+      ],
+    },
   ],
 
   // farbtastic.
@@ -114,219 +315,8 @@ export default [
           sourcemap: true,
         },
       ],
-      plugins: [terser({
-        compress: { inline: false },
-        format: { comments: false },
-      })],
+      plugins: [terser({ compress: { inline: false } })],
     }
-  ],
-
-  // SortableJS.
-  [
-    {
-      ...def,
-      output: { file: 'assets/vendor/sortable/Sortable.js' },
-      plugins: [
-        virtual({ entry: 'import Sortable from "sortablejs/modular/sortable.complete.esm"; export default Sortable;' }),
-        resolve()
-      ],
-    },
-    {
-      ...def,
-      input: 'assets/vendor/sortable/Sortable.js',
-      output: [
-        {
-          file: 'assets/vendor/sortable/Sortable.min.js',
-          sourcemap: true,
-          format: 'iife',
-          name: 'Sortable',
-        },
-      ],
-      plugins: [terser({
-        format: { comments: false },
-      })],
-    }
-  ],
-
-  // jquery-form.
-  [
-    {
-      ...def,
-      output: { file: 'assets/vendor/jquery-form/jquery.form.js' },
-      plugins: [
-        virtual({ entry: 'import "jquery-form";' }),
-        resolve()
-      ],
-    },
-    {
-      ...def,
-      input: 'assets/vendor/jquery-form/jquery.form.js',
-      output: [
-        {
-          file: 'assets/vendor/jquery-form/jquery.form.min.js',
-          sourcemap: true,
-        },
-      ],
-      plugins: [terser({
-        compress: { inline: false },
-        format: { comments: false },
-      })],
-    }
-  ],
-
-  // PopperJS.
-  [
-    {
-      ...def,
-      output: { file: 'assets/vendor/popperjs/popper.js' },
-      plugins: [
-        virtual({ entry: 'import "@popperjs/core/dist/umd/popper";' }),
-        resolve()
-      ],
-    },
-    {
-      ...def,
-      input: 'assets/vendor/popperjs/popper.js',
-      output: [
-        {
-          file: 'assets/vendor/popperjs/popper.min.js',
-          sourcemap: true,
-          format: 'iife',
-          name: 'Popper',
-        },
-      ],
-      plugins: [terser({
-        compress: { inline: false },
-        format: { comments: false },
-      })],
-    }
-  ],
-
-  // js-cookie.
-  [
-    {
-      ...def,
-      output: { file: 'assets/vendor/js-cookie/js.cookie.js' },
-      plugins: [
-        virtual({ entry: 'import Cookies from "js-cookie"; export default Cookies;' }),
-        resolve()
-      ],
-    },
-    {
-      ...def,
-      input: 'assets/vendor/js-cookie/js.cookie.js',
-      output: [
-        {
-          file: 'assets/vendor/js-cookie/js.cookie.min.js',
-          sourcemap: true,
-          format: 'iife',
-          name: 'Cookies',
-        },
-      ],
-      plugins: [terser({ format: { comments: false } })],
-    }
-  ],
-
-  // underscore
-  [
-    {
-      ...def,
-      output: { file: 'assets/vendor/underscore/underscore.js' },
-      plugins: [
-        virtual({ entry: 'import "underscore/underscore";' }),
-        resolve()
-      ],
-    },
-    {
-      ...def,
-      input: 'assets/vendor/underscore/underscore.js',
-      output: [
-        {
-          file: 'assets/vendor/underscore/underscore-min.js',
-          sourcemap: true,
-        },
-      ],
-      plugins: [terser({ format: { comments: false } })],
-    }
-  ],
-
-  // Backbone
-  [
-    {
-      ...def,
-      output: { file: 'assets/vendor/backbone/backbone.js' },
-      plugins: [
-        virtual({ entry: 'import "backbone";' }),
-        resolve()
-      ],
-    },
-    {
-      ...def,
-      input: 'assets/vendor/backbone/backbone.js',
-      output: [
-        {
-          file: 'assets/vendor/backbone/backbone-min.js',
-          sourcemap: true,
-        },
-      ],
-      plugins: [terser({ format: { comments: false } })],
-    }
-  ],
-
-  // jQuery
-  [
-    {
-      ...def,
-      output: { file: 'assets/vendor/jquery/jquery.js' },
-      plugins: [
-        virtual({ entry: 'import "jquery";' }),
-        resolve()
-      ],
-    },
-    {
-      ...def,
-      input: 'assets/vendor/jquery/jquery.js',
-      output: [
-        {
-          file: 'assets/vendor/jquery/jquery.min.js',
-          sourcemap: true,
-        },
-      ],
-      plugins: [
-        terser({
-          // Set compression options to be closer to jquery default output.
-          compress: {
-            evaluate: false,
-            inline: false,
-            passes: 2,
-          },
-          format: { comments: false },
-        }),
-      ],
-    }
-  ],
-
-  // Picturefill.
-  [
-    {
-      ...def,
-      output: { file: 'assets/vendor/picturefill/picturefill.js' },
-      plugins: [
-        virtual({ entry: 'import "picturefill";' }),
-        resolve(),
-      ],
-    },
-    {
-      ...def,
-      input: 'assets/vendor/picturefill/picturefill.js',
-      output: [
-        {
-          file: 'assets/vendor/picturefill/picturefill.min.js',
-          sourcemap: true,
-        },
-      ],
-      plugins: [terser({ format: { comments: false } })],
-    },
   ],
 
 ].flat();
