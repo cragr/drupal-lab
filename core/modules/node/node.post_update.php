bf85fa3b2c05826adfbcb1b8200a037455d31de0
Claudiu Cristea <clau.cristea@gmail.com>
2020-12-03 12:36:47 +0200

    Patch #2765065-101

--- core/modules/node/node.post_update.php
+++ core/modules/node/node.post_update.php
@@ -5,6 +5,8 @@
  * Post update functions for Node.
  */
 
+use Drupal\node\Entity\NodeType;
+
 /**
  * Implements hook_removed_post_updates().
  */
@@ -13,4 +15,18 @@ function node_removed_post_updates() {
     'node_post_update_configure_status_field_widget' => '9.0.0',
     'node_post_update_node_revision_views_data' => '9.0.0',
   ];
+}
+
+/**
+ * Add plural label variants to node-type entities.
+ */
+function node_post_update_plural_variants() {
+  /** @var \Drupal\node\NodeTypeInterface $node_type */
+  foreach (NodeType::loadMultiple() as $node_type) {
+    $node_type
+      ->set('label_singular', NULL)
+      ->set('label_plural', NULL)
+      ->set('label_count', NULL)
+      ->save();
+  }
 }
