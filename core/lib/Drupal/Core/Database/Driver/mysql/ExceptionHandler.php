2c0c599283147bb7dfc5bddd2cc7043f5e4140f2
mondrake <mondrake.org@gmail.com>
2020-12-07 22:25:30 +0100

    First commit

---
+++ core/lib/Drupal/Core/Database/Driver/mysql/ExceptionHandler.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\Core\Database\Driver\mysql;
+
+use Drupal\Core\Database\DatabaseExceptionWrapper;
+use Drupal\Core\Database\ExceptionHandler as BaseExceptionHandler;
+use Drupal\Core\Database\IntegrityConstraintViolationException;
+use Drupal\Core\Database\StatementInterface;
+
+/**
+ * MySql database exception handler class.
+ */
+class ExceptionHandler extends BaseExceptionHandler {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function handleExecutionException(StatementInterface $statement, array $arguments = [], array $options = []) {
+    $throw_exception = $options['throw_exception'] ?? FALSE;
+    if (!$throw_exception) {
+      return;
+    }
+
+    if ($this->exception instanceof \PDOException) {
+      // Wrap the exception in another exception, because PHP does not allow
+      // overriding Exception::getMessage(). Its message is the extra database
+      // debug information.
+      $message = $this->exception->getMessage() . ": " . $statement->getQueryString() . "; " . print_r($arguments, TRUE);
+      $code = is_int($this->exception->getCode()) ? $this->exception->getCode() : 0;
+
+      // SQLSTATE 23xxx errors indicate an integrity constraint violation. Also,
+      // in case of attempted INSERT of a record with an undefined column and no
+      // default value indicated in schema, MySql returns a 1364 error code.
+      if (
+        substr($this->exception->getCode(), -6, -3) == '23' ||
+        ($this->exception->errorInfo[1] ?? NULL) === 1364
+      ) {
+        throw new IntegrityConstraintViolationException($message, $code, $this->exception);
+      }
+      throw new DatabaseExceptionWrapper($message, 0, $this->exception);
+    }
+
+    throw $this->exception;
+  }
+
+}
---
+++ core/lib/Drupal/Core/Database/ExceptionHandler.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Drupal\Core\Database;
+
+/**
+ * Base Database exception handler class.
+ *
+ * This class handles exceptions thrown by the database layer. Database driver
+ * implementation can provide an alternative implementation to support special
+ * handling required by that database.
+ */
+class ExceptionHandler {
+
+  /**
+   * The database connection object.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $connection;
+
+  /**
+   * The exception to be handled.
+   *
+   * @var \Exception
+   */
+  protected $exception;
+
+  /**
+   * Constructs a ExceptionHandler object.
+   *
+   * @param \Drupal\Core\Database\Connection $connection
+   *   The Drupal database connection object.
+   * @param \Exception $exception
+   *   The exception to be handled.
+   */
+  public function __construct(Connection $connection, \Exception $exception) {
+    $this->connection = $connection;
+    $this->exception = $exception;
+  }
+
+  /**
+   * Handles exceptions thrown during the preparation of statement objects.
+   *
+   * @param string $sql
+   *   The SQL statement that was requested to be prepared.
+   * @param array $arguments
+   *   An array of arguments for the prepared statement.
+   * @param array $options
+   *   An associative array of options to control how the database operation is
+   *   run.
+   *
+   * @throws \Drupal\Core\Database\DatabaseExceptionWrapper
+   */
+  public function handleStatementException(string $sql, array $arguments = [], array $options = []): void {
+    $throw_exception = $options['throw_exception'] ?? FALSE;
+    if (!$throw_exception) {
+      return;
+    }
+
+    if ($this->exception instanceof \PDOException) {
+      // Wrap the exception in another exception, because PHP does not allow
+      // overriding Exception::getMessage(). Its message is the extra database
+      // debug information.
+      $message = $this->exception->getMessage() . ": " . $sql . "; " . print_r($arguments, TRUE);
+      throw new DatabaseExceptionWrapper($message, 0, $this->exception);
+    }
+
+    throw $this->exception;
+  }
+
+  /**
+   * Handles exceptions thrown during execution of statement objects.
+   *
+   * @param \Drupal\Core\Database\StatementInterface $statement
+   *   The statement object requested to be executed.
+   * @param array $arguments
+   *   An array of arguments for the prepared statement.
+   * @param array $options
+   *   An associative array of options to control how the database operation is
+   *   run.
+   *
+   * @return \Drupal\Core\Database\StatementInterface|int|null
+   *   Most database drivers will rethrow a Drupal database exception when a PDO
+   *   exception is thrown by a database operation, but some of them may need to
+   *   re-run the query, so they can also return a
+   *   \Drupal\Core\Database\StatementInterface object or an integer.
+   *
+   * @throws \Drupal\Core\Database\DatabaseExceptionWrapper
+   * @throws \Drupal\Core\Database\IntegrityConstraintViolationException
+   */
+  public function handleExecutionException(StatementInterface $statement, array $arguments = [], array $options = []) {
+    $throw_exception = $options['throw_exception'] ?? FALSE;
+    if (!$throw_exception) {
+      return;
+    }
+
+    if ($this->exception instanceof \PDOException) {
+      // Wrap the exception in another exception, because PHP does not allow
+      // overriding Exception::getMessage(). Its message is the extra database
+      // debug information.
+      $message = $this->exception->getMessage() . ": " . $statement->getQueryString() . "; " . print_r($arguments, TRUE);
+      // Match all SQLSTATE 23xxx errors.
+      if (substr($this->exception->getCode(), -6, -3) == '23') {
+        throw new IntegrityConstraintViolationException($message, $this->exception->getCode(), $this->exception);
+      }
+      throw new DatabaseExceptionWrapper($message, 0, $this->exception);
+    }
+
+    throw $this->exception;
+  }
+
+}
