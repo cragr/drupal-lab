66a1ebe7b1b89d1d30e485090c031167be6062e8
mondrake <mondrake.org@gmail.com>
2020-12-08 10:18:37 +0100

    Fix

--- core/lib/Drupal/Core/Database/Driver/mysql/ExceptionHandler.php
+++ core/lib/Drupal/Core/Database/Driver/mysql/ExceptionHandler.php
@@ -26,8 +26,17 @@ class ExceptionHandler extends BaseExceptionHandler {
       // Wrap the exception in another exception, because PHP does not allow
       // overriding Exception::getMessage(). Its message is the extra database
       // debug information.
-      $message = $this->exception->getMessage() . ": " . $statement->getQueryString() . "; " . print_r($arguments, TRUE);
       $code = is_int($this->exception->getCode()) ? $this->exception->getCode() : 0;
+
+      // If a max_allowed_packet error occurs the message length is truncated.
+      // This should prevent the error from recurring if the exception is logged
+      // to the database using dblog or the like.
+      if (($this->exception->errorInfo[1] ?? NULL) === 1153) {
+        $message = Unicode::truncateBytes($this->exception->getMessage(), Connection::MIN_MAX_ALLOWED_PACKET);
+        throw new DatabaseExceptionWrapper($message, $code, $this->exception);
+      }
+
+      $message = $this->exception->getMessage() . ": " . $statement->getQueryString() . "; " . print_r($arguments, TRUE);
 
       // SQLSTATE 23xxx errors indicate an integrity constraint violation. Also,
       // in case of attempted INSERT of a record with an undefined column and no
@@ -37,14 +46,6 @@ class ExceptionHandler extends BaseExceptionHandler {
         ($this->exception->errorInfo[1] ?? NULL) === 1364
       ) {
         throw new IntegrityConstraintViolationException($message, $code, $this->exception);
-      }
-
-      // If a max_allowed_packet error occurs the message length is truncated.
-      // This should prevent the error from recurring if the exception is logged
-      // to the database using dblog or the like.
-      if (($this->exception->errorInfo[1] ?? NULL) === 1153) {
-        $message = Unicode::truncateBytes($this->exception->getMessage(), Connection::MIN_MAX_ALLOWED_PACKET);
-        throw new DatabaseExceptionWrapper($message, $this->exception->getCode(), $this->exception);
       }
 
       throw new DatabaseExceptionWrapper($message, 0, $this->exception);
