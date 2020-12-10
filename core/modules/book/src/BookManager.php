65fdcc8adb1b3b2ee0bc1ef8568a563fa98e1332
Dan Keenan <dk@dankeenan.org>
2020-12-10 15:44:16 -0500

    Use natural sorting when sorting book pages

--- core/modules/book/src/BookManager.php
+++ core/modules/book/src/BookManager.php
@@ -4,13 +4,13 @@ namespace Drupal\book;
 
 use Drupal\Component\Utility\Unicode;
 use Drupal\Core\Cache\Cache;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\RendererInterface;
 use Drupal\Core\Session\AccountInterface;
-use Drupal\Core\StringTranslation\TranslationInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\StringTranslation\TranslationInterface;
 use Drupal\Core\Template\Attribute;
 use Drupal\node\NodeInterface;
 
@@ -976,7 +976,6 @@ class BookManager implements BookManagerInterface {
    *   The book tree to operate on.
    */
   protected function doBookTreeCheckAccess(&$tree) {
-    $new_tree = [];
     foreach ($tree as $key => $v) {
       $item = &$tree[$key]['link'];
       $this->bookLinkTranslate($item);
@@ -984,16 +983,19 @@ class BookManager implements BookManagerInterface {
         if ($tree[$key]['below']) {
           $this->doBookTreeCheckAccess($tree[$key]['below']);
         }
-        // The weights are made a uniform 5 digits by adding 50000 as an offset.
-        // After calling $this->bookLinkTranslate(), $item['title'] has the
-        // translated title. Adding the nid to the end of the index insures that
-        // it is unique.
-        $new_tree[(50000 + $item['weight']) . ' ' . $item['title'] . ' ' . $item['nid']] = $tree[$key];
       }
     }
     // Sort siblings in the tree based on the weights and localized titles.
-    ksort($new_tree);
-    $tree = $new_tree;
+    uasort($tree, function (array $a, array $b): int {
+      $item_a = &$a['link'];
+      $item_b = &$b['link'];
+      if (($cmp_weight = $item_a['weight'] - $item_b['weight']) !== 0) {
+        // Weights differ
+        return $cmp_weight;
+      }
+
+      return strnatcmp($item_a['title'], $item_b['title']);
+    });
   }
 
   /**
