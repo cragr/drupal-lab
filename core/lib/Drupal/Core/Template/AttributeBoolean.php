bf16020911fd95ff455cf99c80420cda70007d5b
mondrake <mondrake.org@gmail.com>
2020-12-13 10:35:33 +0100

    Reverted deprecation triggers

--- core/lib/Drupal/Core/Template/AttributeBoolean.php
+++ core/lib/Drupal/Core/Template/AttributeBoolean.php
@@ -1,8 +1,5 @@
 <?php
 // @codingStandardsIgnoreFile
-
-@trigger_error(__CLASS__ . ' is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Use Drupal\Component\Attribute\AttributeBoolean instead. See https://www.drupal.org/node/3070485', E_USER_DEPRECATED);
-
 /**
  * The original class has been moved to an Attribute component added in Drupal
  * 8.9.0. See the respective component class for full documentation.
