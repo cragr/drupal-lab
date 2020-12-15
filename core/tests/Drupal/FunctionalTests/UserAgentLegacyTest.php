05b1dab03f42c5ad329ed9c4d98626e4ee19f490
voleger <dev.vegera@gmail.com>
2020-12-15 11:46:07 +0200

    Update version in the deprecation messages

--- core/tests/Drupal/FunctionalTests/UserAgentLegacyTest.php
+++ core/tests/Drupal/FunctionalTests/UserAgentLegacyTest.php
@@ -19,11 +19,10 @@ class UserAgentLegacyTest extends BrowserTestBase {
 
   /**
    * Test drupal_valid_test_ua() and drupal_generate_test_ua() functions.
-   *
-   * @expectedDeprecation drupal_valid_test_ua() is deprecated in drupal:9.1.0 and is removed from drupal:10.0.0. Use \Drupal\Core\Test\UserAgent::validate(). See https://www.drupal.org/node/3044173
-   * @expectedDeprecation drupal_generate_test_ua() is deprecated in drupal:9.1.0 and is removed from drupal:10.0.0. Use \Drupal\Core\Test\UserAgent::generate(). See https://www.drupal.org/node/3044173
    */
   public function testDrupalTestUa() {
+    $this->expectDeprecation('drupal_valid_test_ua() is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Use \Drupal\Core\Test\UserAgent::validate(). See https://www.drupal.org/node/3044173');
+    $this->expectDeprecation('drupal_generate_test_ua() is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Use \Drupal\Core\Test\UserAgent::generate(). See https://www.drupal.org/node/3044173');
     $test_prefix = drupal_generate_test_ua(drupal_valid_test_ua());
     $this->assertNotEmpty($test_prefix);
     $this->assertNotFalse(drupal_valid_test_ua($test_prefix));
