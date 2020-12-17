60509074d415787cd069ad9f6d5c3d5dada1db49
xjm <xjm@65776.no-reply.drupal.org>
2020-12-17 09:29:41 -0600

    Use TransferException instead of RequestException where appropriate and document cases where it is not the right fit.

--- core/tests/Drupal/Tests/DrupalTestBrowser.php
+++ core/tests/Drupal/Tests/DrupalTestBrowser.php
@@ -136,8 +136,13 @@ class DrupalTestBrowser extends AbstractBrowser {
     try {
       $response = $this->getClient()->request($method, $uri, $request_options);
     }
+    // Catch RequestException rather than TransferException because we want
+    // to re-throw the exception whenever the response is NULL, and
+    // ConnectException always has a NULL response.
     catch (RequestException $e) {
       $response = $e->getResponse();
+      // @todo RequestException has a hasResponse() method which we could use
+      //   here instead.
       if (NULL === $response) {
         throw $e;
       }
