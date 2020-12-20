c40bccf787877dcaa39680b6cf099ab865f4f67f
Tobias Zimmermann <tstoeckler@gmail.com>
2020-12-20 13:19:08 +0100

    Fix breakage introduced by fix

--- core/lib/Drupal/Core/Entity/Query/Sql/Tables.php
+++ core/lib/Drupal/Core/Entity/Query/Sql/Tables.php
@@ -215,7 +215,6 @@ class Tables implements TablesInterface {
         }
         $entity_tables[$entity_base_table] = $this->getTableMapping($entity_base_table, $entity_type_id);
         $sql_column = $specifier;
-        $columns = $field_storage->getColumns();
 
         // If there are more specifiers, get the right sql column name if the
         // next one is a column of this field.
@@ -243,6 +242,7 @@ class Tables implements TablesInterface {
             $next = $specifiers[$key + 1];
           }
           // Is this a field column?
+          $columns = $field_storage->getColumns();
           if (isset($columns[$next]) || in_array($next, $table_mapping->getReservedColumns())) {
             // Use it.
             $sql_column = $table_mapping->getFieldColumnName($field_storage, $next);
@@ -252,8 +252,11 @@ class Tables implements TablesInterface {
         }
         // If there are no additional specifiers but the field has a main
         // property, use that to look up the column name.
-        elseif ($column && isset($columns[$column])) {
-          $sql_column = $table_mapping->getFieldColumnName($field_storage, $column);
+        elseif ($field_storage && $column) {
+          $columns = $field_storage->getColumns();
+          if (isset($columns[$column])) {
+            $sql_column = $table_mapping->getFieldColumnName($field_storage, $column);
+          }
         }
 
         $table = $this->ensureEntityTable($index_prefix, $sql_column, $type, $langcode, $base_table, $entity_id_field, $entity_tables);
