d3384a732a3e072500c97f64b259ee6ec3881f61
JohnAlbin <virtually.johnalbin@gmail.com>
2021-01-02 14:58:14 +0800

    Rename theme_info to theme_hook_info.

--- core/lib/Drupal/Core/Render/theme.api.php
+++ core/lib/Drupal/Core/Render/theme.api.php
@@ -589,6 +589,7 @@ function hook_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormSta
  *       invoked.
  *     - theme_base_hook: (string) The base hook of the theme hook currently
  *       being invoked.
+ *     - theme_hook_info: (array) The implementation info for the theme hook.
  *     - theme_suggestions: (array) An array of theme hook suggestions.
  *     - Any additional context that was passed from the render array.
  * @param string $hook
@@ -633,6 +634,7 @@ function hook_preprocess(array &$variables, $hook, array $info) {
  *       invoked.
  *     - theme_base_hook: (string) The base hook of the theme hook currently
  *       being invoked.
+ *     - theme_hook_info: (array) The implementation info for the theme hook.
  *     - theme_suggestions: (array) An array of theme hook suggestions.
  *     - Any additional context that was passed from the render array.
  * @param string $hook
@@ -678,6 +680,7 @@ function hook_preprocess_HOOK(array &$variables, $hook, array $info) {
  *       theme suggestions.
  *     - theme_base_hook: (string) The base hook of the theme hook currently
  *       being invoked.
+ *     - theme_hook_info: (array) The implementation info for the theme hook.
  *     - theme_suggestions: (array) The list of theme hook suggestions derived
  *       from the #theme property of the render array.
  *     - Any additional context that was passed from the render array.
@@ -739,6 +742,7 @@ function hook_theme_suggestions_HOOK(array $variables) {
  *       theme suggestions.
  *     - theme_base_hook: (string) The base hook of the theme hook currently
  *       being invoked.
+ *     - theme_hook_info: (array) The implementation info for the theme hook.
  *     - theme_suggestions: (array) The list of theme hook suggestions derived
  *       from the #theme property of the render array.
  *     - Any additional context that was passed from the render array.
@@ -790,6 +794,7 @@ function hook_theme_suggestions_alter(array &$suggestions, array $variables, $ho
  *       theme suggestions.
  *     - theme_base_hook: (string) The base hook of the theme hook currently
  *       being invoked.
+ *     - theme_hook_info: (array) The implementation info for the theme hook.
  *     - theme_suggestions: (array) The list of theme hook suggestions derived
  *       from the #theme property of the render array.
  *     - Any additional context that was passed from the render array.
@@ -875,6 +880,7 @@ function hook_extension() {
  *     invoked.
  *   - theme_base_hook: (string) The base hook of the theme hook currently
  *     being invoked.
+ *   - theme_hook_info: (array) The implementation info for the theme hook.
  *   - theme_suggestions: (array) An array of theme hook suggestions.
  *   - Any additional context that was passed from the render array.
  *
