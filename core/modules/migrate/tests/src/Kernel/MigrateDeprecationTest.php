74e42ba48b1a8671141009add56715934b1fe9df
Claudiu Cristea <clau.cristea@gmail.com>
2021-01-06 19:02:27 +0200

    Use ::setUpdate() instead of ::prepareUpdate()

--- core/modules/migrate/tests/src/Kernel/MigrateDeprecationTest.php
+++ core/modules/migrate/tests/src/Kernel/MigrateDeprecationTest.php
@@ -29,7 +29,7 @@ class MigrateDeprecationTest extends KernelTestBase {
     /** @var \Drupal\migrate\Plugin\MigratePluginManagerInterface $manager */
     $manager = $this->container->get('plugin.manager.migrate.id_map');
     $map = $manager->createInstance('null');
-    $this->expectDeprecation('NullIdMap::prepareUpdate() is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Use \Drupal\Tests\migrate\Traiths\MigrateTestTrait::prepareUpdate() in tests. See https://www.drupal.org/node/3188673');
+    $this->expectDeprecation('NullIdMap::prepareUpdate() is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Use \Drupal\migrate\Plugin\MigrateIdMapInterface::setUpdate() with no parameter instead. See https://www.drupal.org/node/3188673');
     $map->prepareUpdate();
   }
 
@@ -42,7 +42,7 @@ class MigrateDeprecationTest extends KernelTestBase {
     /** @var \Drupal\migrate\Plugin\MigrationInterface $migration */
     $migration = $manager->createInstance('sample_stubbing_migration');
     $map = Sql::create($this->container, [], 'sql', [], $migration);
-    $this->expectDeprecation('Sql::prepareUpdate() is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Use \Drupal\Tests\migrate\Traiths\MigrateTestTrait::prepareUpdate() in tests. See https://www.drupal.org/node/3188673');
+    $this->expectDeprecation('Sql::prepareUpdate() is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Use \Drupal\migrate\Plugin\MigrateIdMapInterface::setUpdate() with no parameter instead. See https://www.drupal.org/node/3188673');
     $map->prepareUpdate();
   }
 
