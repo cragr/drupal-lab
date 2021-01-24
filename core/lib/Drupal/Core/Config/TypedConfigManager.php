b914f18785ab1e4f0b260313515803b3930e65fd
anmolgoyal74 <anmol.g@opensenselabs.com>
2021-01-24 22:26:01 +0530

    Changed drupal logger with trigger error

--- core/lib/Drupal/Core/Config/TypedConfigManager.php
+++ core/lib/Drupal/Core/Config/TypedConfigManager.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Core\Config;
 
+use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Config\Schema\ConfigSchemaAlterException;
@@ -78,9 +79,9 @@ class TypedConfigManager extends TypedDataManager implements TypedConfigManagerI
     if ($data === FALSE) {
       // For a typed config the data MUST exist.
       $data = [];
-      \Drupal::logger('config_sync')->error('Missing required data for typed configuration: %config', [
-        '%config' => $name,
-      ]);
+      trigger_error(new FormattableMarkup('Missing required data for typed configuration: @config', [
+        '@config' => $name,
+      ]), E_USER_ERROR);
     }
     return $this->createFromNameAndData($name, $data);
   }
