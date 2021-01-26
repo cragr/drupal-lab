5b67de14b0d45325b2c38aac24d56702e15c3bc1
mondrake <mondrake.org@gmail.com>
2021-01-26 11:38:33 +0100

    Fix

--- core/lib/Drupal/Core/Database/Query/Select.php
+++ core/lib/Drupal/Core/Database/Query/Select.php
@@ -322,7 +322,7 @@ class Select extends Query implements SelectInterface {
     $base_class = end($parts);
     $driver_class = $this->connection->getDriverClass($base_class);
     $class = $driver_class !== $base_class ? $driver_class : $extender_name;
-    if ($class === PagerSelectExtender::class || is_subclass_of($class, PagerSelectExtender::class)) {
+    if (strpos($class, PagerSelectExtender::class) !== FALSE || is_subclass_of($class, PagerSelectExtender::class)) {
       return new $class($this, $this->connection, \Drupal::service('pager.manager'));
     }
     else {
