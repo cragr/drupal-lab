ab753a601e35d0e9bf399f21c82a3706216595d6
quietone <quietone@2572884.no-reply.drupal.org>
2021-02-06 02:38:51 +1300

    Set default value for messenger to NULL

--- core/modules/user/src/Authentication/Provider/Cookie.php
+++ core/modules/user/src/Authentication/Provider/Cookie.php
@@ -52,15 +52,16 @@ class Cookie implements AuthenticationProviderInterface, EventSubscriberInterfac
    *   The session configuration.
    * @param \Drupal\Core\Database\Connection $connection
    *   The database connection.
-   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   * @param \Drupal\Core\Messenger\MessengerInterface|null $messenger
    *   The messenger.
    */
-  public function __construct(SessionConfigurationInterface $session_configuration, Connection $connection, MessengerInterface $messenger) {
+  public function __construct(SessionConfigurationInterface $session_configuration, Connection $connection, MessengerInterface $messenger = NULL) {
     $this->sessionConfiguration = $session_configuration;
     $this->connection = $connection;
     $this->messenger = $messenger;
     if ($this->messenger === NULL) {
       @trigger_error('The MessengerInterface must be passed to ' . __NAMESPACE__ . '\Cookie::__construct(). It was added in drupal:9.2.0 and will be required before drupal:10.0.0.', E_USER_DEPRECATED);
+      $this->messenger = \Drupal::messenger();
     }
   }
 
