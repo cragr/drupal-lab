c5be534ad8673e12388d04935abdde127c07a4c1
Ted Bowman <ted@tedbow.com>
2021-02-12 12:10:06 -0500

    remove attempto set config in TestSiteInstallTestScript

--- core/tests/Drupal/TestSite/TestSiteInstallTestScript.php
+++ core/tests/Drupal/TestSite/TestSiteInstallTestScript.php
@@ -13,13 +13,7 @@ class TestSiteInstallTestScript implements TestSetupInterface {
    * {@inheritdoc}
    */
   public function setup() {
-    \Drupal::service('module_installer')->install(['test_page_test', 'advisory_feed_test']);
-    // Make sure tests do not make service advisory fetching unless the test
-    // opts in.
-    \Drupal::configFactory()
-      ->getEditable('system.advisories')
-      ->set('enabled', FALSE)
-      ->save();
+    \Drupal::service('module_installer')->install(['test_page_test']);
   }
 
 }
