d083c44666c69c269a910b4641588ec43b8fa5ac
SUDIPTA DAS <dsudipta2012@gmail.com>
2021-02-24 13:15:12 +0530

    replace @expectedException and @expectedExceptionMessage by ->expectException() and ->expectExceptionMessage()

--- core/modules/contact/tests/src/Unit/ContactTest.php
+++ core/modules/contact/tests/src/Unit/ContactTest.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\Tests\contact\Unit\ContactTest.
- */
-
 namespace Drupal\Tests\contact\Unit;
 
 use Drupal\Tests\UnitTestCase;
@@ -23,14 +18,14 @@ class ContactTest extends UnitTestCase {
 
   /**
    * Test that contact_menu_local_tasks_alter throws Undefined index: tabs.
-   *
-   * @expectedException PHPUnit\Framework\Error\Notice
-   * @expectedExceptionMessage Undefined index: tabs
    */
   public function testLocalTasksAlter() {
     include 'modules/contact/contact.module';
     $data = [];
     $route_name = 'entity.user.canonical';
+    $this->expectException('\PHPUnit\Framework\Error\Notice');
+    $this->expectExceptionMessage('Undefined index: tabs');
     \contact_menu_local_tasks_alter($data, $route_name);
   }
+
 }
