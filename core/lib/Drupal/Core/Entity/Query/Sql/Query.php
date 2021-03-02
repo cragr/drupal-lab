01804887223b955b26093ead3266dc1f4cbd5515
Jonathan Shaw <jonathanjfshaw@gmail.com>
2021-03-02 21:40:22 +0000

    Leave access checking as true by default

--- core/lib/Drupal/Core/Entity/Query/Sql/Query.php
+++ core/lib/Drupal/Core/Entity/Query/Sql/Query.php
@@ -139,7 +139,7 @@ class Query extends QueryBase implements QueryInterface {
     if (is_null($this->accessCheck)) {
       @trigger_error('Omitting to explicitly specify whether access should be checked on an entity query is deprecated in drupal:9.2.0. An error will be thrown from drupal:10.0.0. See https://www.drupal.org/node/3201242', E_USER_DEPRECATED);
     }
-    if ($this->accessCheck) {
+    if ($this->accessCheck || is_null($this->accessCheck)) {
       $this->sqlQuery->addTag($this->entityTypeId . '_access');
     }
     $this->sqlQuery->addTag('entity_query');
