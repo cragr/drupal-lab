933ce4cb225018d7f6d2c7a209e35628f6131d6e
jonathanshaw <jonathanjfshaw@3237629.no-reply.drupal.org>
2021-03-20 22:38:52 +0000

    Expectations in correct sequence

--- core/tests/Drupal/Tests/Core/Menu/DefaultMenuLinkTreeManipulatorsTest.php
+++ core/tests/Drupal/Tests/Core/Menu/DefaultMenuLinkTreeManipulatorsTest.php
@@ -292,11 +292,11 @@ class DefaultMenuLinkTreeManipulatorsTest extends UnitTestCase {
 
     $query = $this->createMock('Drupal\Core\Entity\Query\QueryInterface');
     $query->expects($this->at(0))
-      ->method('condition')
-      ->with('nid', [1, 2, 3, 4]);
-    $query->expects($this->at(1))
       ->method('accessCheck')
       ->with(TRUE);
+    $query->expects($this->at(1))
+      ->method('condition')
+      ->with('nid', [1, 2, 3, 4]);
     $query->expects($this->at(2))
       ->method('condition')
       ->with('status', NodeInterface::PUBLISHED);
