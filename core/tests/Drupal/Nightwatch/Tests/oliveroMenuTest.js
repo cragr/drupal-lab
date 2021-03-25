07b435689a3db5dd885485c059648f7a598d1aaa
mglaman <mglaman@2416470.no-reply.drupal.org>
2021-03-25 19:49:16 +0000

    Add menu test

---
+++ core/tests/Drupal/Nightwatch/Tests/oliveroMenuTest.js
@@ -0,0 +1,27 @@
+module.exports = {
+  '@tags': ['core', 'olivero'],
+  before(browser) {
+    browser.drupalInstall({
+      setupFile: 'core/tests/Drupal/TestSite/TestSiteOliveroInstallTestScript.php',
+      installProfile: 'standard',
+    });
+    browser.resizeWindow(1400, 800);
+  },
+  after(browser) {
+    browser.drupalUninstall();
+  },
+  'On scroll, menu collapses to burger 🍔 menu': (browser) => {
+    browser
+    .drupalRelativeURL('/')
+      .assert.containsText(
+        '#block-olivero-content h2',
+        'Congratulations and welcome to the Drupal community!',
+      );
+
+    browser.assert.not.visible('button.wide-nav-expand');
+    browser.getLocationInView('footer.site-footer', function () {
+      this.assert.visible('button.wide-nav-expand');
+      this.assert.not.visible('#site-header__inner');
+    });
+  },
+};
---
+++ core/tests/Drupal/TestSite/TestSiteOliveroInstallTestScript.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\TestSite;
+
+use Drupal\Core\Extension\ModuleInstallerInterface;
+use Drupal\Core\Extension\ThemeInstallerInterface;
+
+/**
+ * Setup file used by TestSiteInstallTestScript.
+ *
+ * @see \Drupal\Tests\Scripts\TestSiteApplicationTest
+ */
+class TestSiteInstallTestScript implements TestSetupInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setup() {
+    // @todo Right now we need Standard install for dependencies, figure them out here.
+    // $module_installer = \Drupal::service('module_installer');
+    // assert($module_installer instanceof ModuleInstallerInterface);
+    // $module_installer->install(['block', 'views']);
+    $theme_installer = \Drupal::service('theme_installer');
+    assert($theme_installer instanceof ThemeInstallerInterface);
+    $theme_installer->install(['olivero'], TRUE);
+    $system_theme_config = \Drupal::configFactory()->getEditable('system.theme');
+    $system_theme_config->set('default', 'olivero')->save();
+  }
+
+}
