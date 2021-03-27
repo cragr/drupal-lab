66af4fdf2826f843447675d8b8e0f20efe30d5bc
Benji Fisher <benji@FisherFam.org>
2021-03-27 10:22:55 -0400

    Add links to the Main menu from a test module

--- core/tests/Drupal/TestSite/TestSiteOliveroInstallTestScript.php
+++ core/tests/Drupal/TestSite/TestSiteOliveroInstallTestScript.php
@@ -20,13 +20,7 @@ class TestSiteOliveroInstallTestScript implements TestSetupInterface {
     // Install required module for the Olivero front page.
     $module_installer = \Drupal::service('module_installer');
     assert($module_installer instanceof ModuleInstallerInterface);
-    $module_installer->install(['block', 'views']);
-
-    // Add a link to the main menu.
-    $menu_link_manager = \Drupal::service('plugin.manager.menu.link');
-    assert($menu_link_manager instanceof MenuLinkManager);
-    $link_properties = ['title' => 'Home', 'route_name' => '<front>', 'menu_name' => 'main'];
-    $menu_link_manager->addDefinition('olivero.front_page', $link_properties);
+    $module_installer->install(['block', 'views', 'olivero_test']);
 
     // Install Olivero and set it as the default theme.
     $theme_installer = \Drupal::service('theme_installer');
