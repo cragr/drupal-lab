4cfa71fbea81490112435f0ec27d8754181e93ef
Scott Euser <scotteuser@gmail.com>
2021-03-30 15:42:09 +0100

    Update taxonomy relationship test to apply to latest 9.2.x

--- core/modules/taxonomy/tests/src/Functional/Views/TaxonomyRelationshipTest.php
+++ core/modules/taxonomy/tests/src/Functional/Views/TaxonomyRelationshipTest.php
@@ -83,7 +83,7 @@ class TaxonomyRelationshipTest extends TaxonomyTestBase {
     $this->assertEqual('standard', $views_data['parent_target_id']['relationship']['id']);
     // Check the parent filter and argument data.
     $this->assertEqual('numeric', $views_data['parent_target_id']['filter']['id']);
-    $this->assertEqual('taxonomy', $views_data['parent_target_id']['argument']['id']);
+    $this->assertEqual('entity_reference', $views_data['parent_target_id']['argument']['id']);
 
     // Check an actual test view.
     $view = Views::getView('test_taxonomy_term_relationship');
