a15f2587909d982fe43270256488b625da7603ca
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2021-03-31 21:52:11 +0100

    Added definition of DRUPAL_ROOT to the actual autoload.php file.

--- autoload.php
+++ autoload.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Includes the autoloader created by Composer.
+ * Includes the autoloader created by Composer and defines DRUPAL_ROOT.
  *
  * @see composer.json
  * @see index.php
@@ -10,5 +10,14 @@
  * @see core/rebuild.php
  * @see core/modules/statistics/statistics.php
  */
+
+/**
+ * Defines the root directory of the Drupal installation.
+ *
+ * Defining the DRUPAL_ROOT constant here ensures that it holds the actual
+ * location of the webroot (where this file is placed scaffolding), even if the
+ * rest of Drupal is symlinked into the Composer project.
+ */
+define('DRUPAL_ROOT', __DIR__);
 
 return require __DIR__ . '/vendor/autoload.php';
