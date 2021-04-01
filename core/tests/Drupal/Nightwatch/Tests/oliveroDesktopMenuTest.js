259a6b519f74423408a732dbad19b7cf05aa0087
Mike Herchel <mike@lullabot.com>
2021-04-01 14:51:04 -0400

    Refactorign of desktop functionality test.

--- core/tests/Drupal/Nightwatch/Tests/oliveroDesktopMenuTest.js
+++ core/tests/Drupal/Nightwatch/Tests/oliveroDesktopMenuTest.js
@@ -1,6 +1,3 @@
-// Nightwatch suggests non-ES6 functions when using the execute method.
-// eslint-disable func-names, prefer-arrow-callback
-
 const headerNavSelector = '#header-nav';
 const linkSubMenuId = 'home-submenu-1';
 const buttonSubMenuId = 'button-submenu-2';
@@ -18,24 +15,23 @@ module.exports = {
   after(browser) {
     browser.drupalUninstall();
   },
-  'Verify desktop menu click functionality': (browser) => {
+  'Verify Olivero desktop menu click functionality': (browser) => {
     browser
       .drupalRelativeURL('/node')
-      .assert.visible(headerNavSelector)
+      .waitForElementVisible(headerNavSelector)
       .assert.not.visible(`#${linkSubMenuId}`)
       .assert.attributeEquals(
         `[aria-controls="${linkSubMenuId}"]`,
         'aria-expanded',
         'false',
       )
-      .click(`[aria-controls="${linkSubMenuId}"]`, () => {
-        browser.assert.visible(`#${linkSubMenuId}`);
-        browser.assert.attributeEquals(
-          `[aria-controls="${linkSubMenuId}"]`,
-          'aria-expanded',
-          'true',
-        );
-      })
+      .click(`[aria-controls="${linkSubMenuId}"]`)
+      .assert.visible(`#${linkSubMenuId}`)
+      .assert.attributeEquals(
+        `[aria-controls="${linkSubMenuId}"]`,
+        'aria-expanded',
+        'true',
+      )
 
       // Test interactions for route:<button> menu links.
       .assert.not.visible(`#${buttonSubMenuId}`)
@@ -44,37 +40,32 @@ module.exports = {
         'aria-expanded',
         'false',
       )
-      .click(`[aria-controls="${buttonSubMenuId}"]`, () => {
-        browser.assert.visible(`#${buttonSubMenuId}`);
-        browser.assert.attributeEquals(
-          `[aria-controls="${buttonSubMenuId}"]`,
-          'aria-expanded',
-          'true',
-        );
-      });
+      .click(`[aria-controls="${buttonSubMenuId}"]`)
+      .assert.visible(`#${buttonSubMenuId}`)
+      .assert.attributeEquals(
+        `[aria-controls="${buttonSubMenuId}"]`,
+        'aria-expanded',
+        'true',
+      );
   },
-  'Verify desktop menu hover functionality': (browser) => {
+  'Verify Olivero desktop menu hover functionality': (browser) => {
     browser
       .drupalRelativeURL('/node')
-      .waitForElementVisible('body', 1000, () => {
-        browser.assert
-          .visible(headerNavSelector)
-          .moveToElement('link text', 'home', () => {
-            browser.assert.visible(`#${linkSubMenuId}`);
-            browser.assert.attributeEquals(
-              `[aria-controls="${linkSubMenuId}"]`,
-              'aria-expanded',
-              'true',
-            );
-          })
-          .moveToElement('link text', 'button', () => {
-            browser.assert.visible(`#${buttonSubMenuId}`);
-            browser.assert.attributeEquals(
-              `[aria-controls="${buttonSubMenuId}"]`,
-              'aria-expanded',
-              'true',
-            );
-          });
-      });
+      .waitForElementVisible(headerNavSelector)
+      .assert.visible(headerNavSelector)
+      .moveToElement('link text', 'home')
+      .assert.visible(`#${linkSubMenuId}`)
+      .assert.attributeEquals(
+        `[aria-controls="${linkSubMenuId}"]`,
+        'aria-expanded',
+        'true',
+      )
+      .moveToElement('link text', 'button')
+      .assert.visible(`#${buttonSubMenuId}`)
+      .assert.attributeEquals(
+        `[aria-controls="${buttonSubMenuId}"]`,
+        'aria-expanded',
+        'true',
+      );
   },
 };
