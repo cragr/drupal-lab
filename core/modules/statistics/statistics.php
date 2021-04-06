1bfc5efb3cc061ce0d10857aa138c97d25fe60f2
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2021-04-06 16:17:03 +0100

    Fixed statistics.php to load the scaffolded autoloader and so get the correct DRUPAL_ROOT set.

--- core/modules/statistics/statistics.php
+++ core/modules/statistics/statistics.php
@@ -8,7 +8,10 @@
 use Drupal\Core\DrupalKernel;
 use Symfony\Component\HttpFoundation\Request;
 
-chdir('../../..');
+// Change the directory to the Drupal root. Use SCRIPT_FILENAME rather than
+// __DIR__ to allow for the case where files are symlinked into the webroot.
+$root_path = dirname($_SERVER['SCRIPT_FILENAME'], 4);
+chdir($root_path);
 
 $autoloader = require_once 'autoload.php';
 
