edc3ef8d56070ff505b613578480d67f3e907e12
Frank Ebbers <sheetmusicfrank!@gmail.com>
2021-04-11 13:58:43 +0200

    Quietone's suggestions strike again - Round 5

--- core/modules/views/src/Plugin/views/filter/StringFilter.php
+++ core/modules/views/src/Plugin/views/filter/StringFilter.php
@@ -93,7 +93,7 @@ class StringFilter extends FilterPluginBase {
   }
 
   /**
-   * Operators.
+   * Get operators.
    *
    * This kind of construct makes it relatively easy for a child class
    * to add or remove functionality by overriding this function and
