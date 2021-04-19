79d12e1b6b210afe30d36310a11a90e6168a806e
jonathanshaw <jonathanjfshaw@3237629.no-reply.drupal.org>
2021-04-19 20:01:06 +0000

    Remove debugging code

--- core/lib/Drupal/Core/Entity/Query/Sql/Query.php
+++ core/lib/Drupal/Core/Entity/Query/Sql/Query.php
@@ -138,8 +138,7 @@ class Query extends QueryBase implements QueryInterface {
 
     if (is_null($this->accessCheck)) {
       $this->accessCheck(TRUE);
-      $e = new \Exception('access check not set');
-      @trigger_error($e->getTraceAsString() . 'Omitting to explicitly specify whether access should be checked on an entity query is deprecated in drupal:9.2.0. An error will be thrown from drupal:10.0.0. See https://www.drupal.org/node/3201242', E_USER_DEPRECATED);
+      @trigger_error('Omitting to explicitly specify whether access should be checked on an entity query is deprecated in drupal:9.2.0. An error will be thrown from drupal:10.0.0. See https://www.drupal.org/node/3201242', E_USER_DEPRECATED);
     }
     if ($this->accessCheck) {
       $this->sqlQuery->addTag($this->entityTypeId . '_access');
