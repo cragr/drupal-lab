000d0ddee2592f17c40cb1adebebc7ef771159fb
mondrake <mondrake.org@gmail.com>
2021-04-22 21:51:48 +0200

    Fixes

--- core/tests/Drupal/KernelTests/Core/Database/SelectTest.php
+++ core/tests/Drupal/KernelTests/Core/Database/SelectTest.php
@@ -556,9 +556,8 @@ class SelectTest extends DatabaseTestBase {
    * @group legacy
    */
   public function testLegacyThrowExceptionOption() {
-    $this->expectDeprecation("Passing a 'throw_exception' option to Drupal\Core\Database\Driver\mysql\ExceptionHandler::handleExecutionException is deprecated in drupal:9.2.0 and is removed in drupal:10.0.0. Always catch exceptions. See https://www.drupal.org/node/3201187");
+    $this->expectDeprecation("Passing a 'throw_exception' option to %AExceptionHandler::handleExecutionException is deprecated in drupal:9.2.0 and is removed in drupal:10.0.0. Always catch exceptions. See https://www.drupal.org/node/3201187");
     // This query will fail because the table does not exist.
-    $options['throw_exception'] = FALSE;
     $this->assertNull($this->connection->select('some_table_that_does_not_exist', 't', ['throw_exception' => FALSE])
       ->fields('t')
       ->countQuery()
